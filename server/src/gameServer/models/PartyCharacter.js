5933799c418d45b8c46d003d5ac33d24b7a4c7a6
Alvydas Poskevicius <alvyxaz@gmail.com>
2015-06-26 08:17:42 +0300

    Cleanup refactored files

--- server/src/gameServer/models/PartyCharacter.js
+++
@@ -1,7 +0,0 @@
-var PartyCharacter = (function () {
-    function PartyCharacter() {
-    }
-    return PartyCharacter;
-})();
-module.exports = PartyCharacter;
-//# sourceMappingURL=PartyCharacter.js.map
--- server/src/gameServer/models/PartyCharacter.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Alvys on 2015-06-24.
- */
-class PartyCharacter {
-
-}
-
-export = PartyCharacter;
